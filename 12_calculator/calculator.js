d81c892cde55105264ab74243bed05740a82d485
Zarifa <mamadalievazara@gmail.com>
2025-08-23 22:41:53 +0200

    Fix: caclculator task by deleting parameteres provided by me

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -2,34 +2,27 @@ const add = function(a, b) {
 	return a + b;
 };
 
-add(22, 13)
 
 const subtract = function(a, b) {
 	return a - b;
 };
 
-subtract(45 - 31)
 
 const sum = function(numbers) {
 	return numbers.reduce((acc, current) => acc + current, 0);
 };
 
-sum([1,2,3])
 
 const multiply = function(numbers) {
-  return a * b;
-  //or return numbers.reduce((acc, current) => acc * current, 0); 
+  //return a * b;
+  return numbers.reduce((acc, current) => acc * current, 0); 
 };
 
-multiply(35, 7)
 
 const power = function(a, b) {
 	//return Math.pow(a, b);
 	return a ** b;
 };
-
-power(5, 3)
-
 
 
 const factorial = function(n) {
@@ -38,7 +31,7 @@ const factorial = function(n) {
     result *= i;
 };
 
-factorial(5)
+
 
 // Do not edit below this line
 module.exports = {
