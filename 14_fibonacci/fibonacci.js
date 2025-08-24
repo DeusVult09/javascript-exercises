0c1ed883151c7fa770f770ce2acc168d088f23f6
Zarifa <mamadalievazara@gmail.com>
2025-08-24 22:43:24 +0200

    Finish: ex.14 with 3 possible solutions: using for, .map and .reduce loop methods

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+
+    const fibonacci = function(n) {
+    let count
+    if (typeof n !== 'number') {
+        count = parseInt(n)
+    } else {
+        count = n;
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+
+    const fib = [1, 1];
+    for (let i = 2; i < count; i++) {
+        fib[i] = fib[i - 1] + fib [i - 2];    
+    }
+
+    return fib[count - 1];
 
 };
 
--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,50 @@
-const getTheTitles = function() {
 
-};
+
+/*const getTheTitles = function(arr) {
+  let valueArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      let currentValue = arr[i].title;
+      valueArr.push(currentValue);
+    }
+  return valueArr;
+};*/
+
+/*const getTheTitles = function(obj) {
+  let valueArr = obj.map(item => item.title);
+  return valueArr; 
+}*/
+
+const getTheTitles = function(arr) {
+  return arr.reduce((titles, book) => {
+    titles.push(book.title);
+    return titles;
+  }, [])  
+}
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+
+getTheTitles(books);
+
+
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
 // Do not edit below this line
 module.exports = getTheTitles;
